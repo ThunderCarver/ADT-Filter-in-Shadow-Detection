472e1b47825fb7ce0c8cde0adbce6a80388e946b
jshatusdem <lavitanove@gmail.com>
2020-11-18 11:41:09 +0800

    Create testAE.m

---
+++ ADT/models/testAE.m
@@ -0,0 +1,26 @@
+for iter=1:7
+    datapath=cd;
+    datapath=strcat(fileparts(datapath),'/cross validation/',num2str(iter));
+    load (strcat(datapath,'/Ttestdata.mat'));
+    traindata=Ttestdata';
+    mlplabel=TtestLabel';
+    a=find(mlplabel==-1);
+    mlplabel=cat(1,mlplabel,zeros(1,length(mlplabel)));
+    mlplabel(2,a)=1;  %down is non-shadow label
+    mlplabel(1,a)=0;  %up is shadow label
+    x=traindata;
+    t = mlplabel; 
+    load (strcat(datapath,'/AE10satCV.mat'));
+    y=AE10satCV(x);
+%     figure,plotconfusion(t,y);
+%     saveas(gcf,strcat(datapath,'/AE2010satCV-test.png'));
+%     close(gcf);
+    %% plot ROC 
+    [axis_x,axis_y,threhold,auc]=perfcurve(TtestLabel,y(1,:)',1);
+    figure,plot(axis_x,axis_y);
+    xlabel('False Positive Rate');
+    ylabel('True Positive Rate');
+    title(['ROC curve of (AUC = ' num2str(auc) ' )']);
+    saveas(gcf,strcat(datapath,'/AE10ROC.fig'));
+%     fprintf('auc=%d...',auc);
+end
